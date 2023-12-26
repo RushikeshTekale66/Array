b5ca40e846657b65de728f3aa651773541fcd28b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-26 22:45:36 +0530

    Check given number is prensnt in Array at which index

--- array.java
+++ array.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class array {
+class array{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of an array");
@@ -14,5 +14,18 @@ class array {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i]+" ");
         }
+        
+        System.out.println();
+
+        System.out.println("Enter the number you want to check at which index it is present ");
+        int number = sc.nextInt();
+
+        int index = 0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==number){
+                index = i;
+            }
+        }
+        System.out.println("The number" + number + " is present at index : "+ index);
     }
 }
