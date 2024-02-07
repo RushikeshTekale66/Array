a532db50814d3d69d258c671bc5897c7cde0737c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-07 20:44:38 +0530

    Arrays Method : Arrays.sort()

--- array.java
+++ array.java
@@ -1,11 +1,23 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-            //Declaring two dimentional array
-        int[][] matrix = new int[3][3]; // 3x3 matrix
+        //it will create an array of size 10 and value 0
+        int [] arr1 = new int[10];
+
+        //it will create an array and assign value to it
+        int [] arr2 = {1,3,4,5,1,2,3,4,5,6};
+
+        //Sort Array
+        Arrays.sort(arr2);
+
+        //access element from an array
+        for(int i=0; i<arr2.length; i++){
+            System.out.print(arr2[i]+" ");
+        }
 
     }
 }
