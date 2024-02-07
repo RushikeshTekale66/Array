21a14fc1fc47a127575d7a870b6f0c49cab6d838
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-07 21:00:31 +0530

    Arrays Method: Arrays.fill()

--- array.java
+++ array.java
@@ -5,20 +5,18 @@ class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        //it will create an array of size 10 and value 0
-        int [] arr1 = new int[10];
+        // it will create an array of size 10 and value 0
+        int[] arr1 = new int[10];
 
-        //it will create an array and assign value to it
-        int [] arr2 = {1,3,4,5,1,2,3,4,5,6};
+        // it will create an array and assign value to it
+        int[] arr2 = { 1, 3, 4, 5, 1, 2, 3, 4, 5, 6 };
 
-        int key = 5;
-        // searches the index of key in array
-        int index = Arrays.binarySearch(arr2, key);
-        System.out.println(index);
+        // Fill the array with value 10
+        Arrays.fill(arr2, 10);
 
-        //access element from an array
-        for(int i=0; i<arr2.length; i++){
-            System.out.println("Index is " +i+ ", element is "+arr2[i]);
+        // access element from an array
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println("Index is " + i + ", element is " + arr2[i]);
         }
 
     }
