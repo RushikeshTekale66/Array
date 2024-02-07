7fd7dfcaa9d2d02db91d3dc523f4ce16d8597846
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-07 21:32:10 +0530

    Arrays Methos: Arrays.toString(arr)

--- array.java
+++ array.java
@@ -5,15 +5,11 @@ class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] array1 = { 1, 2, 3 };
-        int[] array2 = { 1, 2, 3 };
-        int[] array3 = { 1, 2, 4 };
+        // The toString() method returns a string representation of the contents of the
+        // specified array.
+        int[] numbers = { 1, 2, 3, 4, 5 };
 
-        boolean isEqual1And2 = Arrays.equals(array1, array2); // true
-        boolean isEqual1And3 = Arrays.equals(array1, array3); // false
-
-        System.out.println("Arrays array1 and array2 are equal: " + isEqual1And2);
-        System.out.println("Arrays array1 and array3 are equal: " + isEqual1And3);
-
+        String arrayString = Arrays.toString(numbers);
+        System.out.println("Array: " + arrayString); // Output: Array: [1, 2, 3, 4, 5]
     }
 }
