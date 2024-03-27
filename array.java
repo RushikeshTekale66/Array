1944db10cfa6dc18d0e3a4adb43d0cbb118ee62c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-03-27 23:44:08 +0530

    Print all array element

--- array.java
+++ array.java
@@ -5,11 +5,12 @@ class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // The toString() method returns a string representation of the contents of the
-        // specified array.
         int[] numbers = { 1, 2, 3, 4, 5 };
 
-        String arrayString = Arrays.toString(numbers);
-        System.out.println("Array: " + arrayString); // Output: Array: [1, 2, 3, 4, 5]
+        for(int i=0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+        
     }
 }
