d893cdc4bd82ced5bac09040709ede4ccf91b72a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-04-03 22:48:12 +0530

    Check the number at which index in array

--- array.java
+++ array.java
@@ -7,10 +7,12 @@ class array {
 
         int[] numbers = { 1, 2, 3, 4, 5 };
 
+        int number = 5;
         for(int i=0; i<numbers.length; i++){
-            System.out.println(numbers[i]);
+           if(number==numbers[i]){
+            System.out.println("Element is present at index : " + i);
+           }
+           else{System.out.println("Element is not present");}
         }
-
-        
     }
 }
