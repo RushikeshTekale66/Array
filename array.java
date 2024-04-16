2b2d8561e6c188b21bc90b65cb985076a6c75897
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-04-16 21:12:08 +0530

    Array sorting using for loop

--- array.java
+++ array.java
@@ -5,9 +5,20 @@ class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] numbers = { 1, 2, 3, 4, 5 };
+        int[] numbers = { 1,3,4,6,5,2,1 };
 
-        int number = 5;
-        System.out.println(numbers[numbers.length-1]);
+        for(int i=0; i<numbers.length-1; i++){
+            for(int j=i+1; j<numbers.length; j++){
+                if(numbers[i]>numbers[j]){
+                int temp = numbers[i];
+                numbers[i]=numbers[j];
+                numbers[j]=temp;
+                }
+
+            }
+        }
+        for(int i=0;i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
     }
 }
