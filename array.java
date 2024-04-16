7f4b3bba30db57926105c49c6d8c951f882e0676
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-04-16 21:17:26 +0530

    Array sorting using Arrays.sort()

--- array.java
+++ array.java
@@ -6,17 +6,7 @@ class array {
         Scanner sc = new Scanner(System.in);
 
         int[] numbers = { 1,3,4,6,5,2,1 };
-
-        for(int i=0; i<numbers.length-1; i++){
-            for(int j=i+1; j<numbers.length; j++){
-                if(numbers[i]>numbers[j]){
-                int temp = numbers[i];
-                numbers[i]=numbers[j];
-                numbers[j]=temp;
-                }
-
-            }
-        }
+        Arrays.sort(numbers);
         for(int i=0;i<numbers.length; i++){
             System.out.println(numbers[i]);
         }
