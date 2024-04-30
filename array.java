2ce47d8d2ba37b5f87f2628e00124a92dceb61fb
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-04-30 21:24:24 +0530

    Get the array element & find the occurance of element in array

--- array.java
+++ array.java
@@ -5,10 +5,24 @@ class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] numbers = { 1,3,4,6,5,2,1 };
-        Arrays.sort(numbers);
-        for(int i=0;i<numbers.length; i++){
-            System.out.println(numbers[i]);
+        System.out.println("Enter the array length");
+        int arrlength = sc.nextInt();
+        int [] arr = new int[arrlength];
+
+        System.out.println("Enter the array element form 1-9");
+        for(int i=0; i<arrlength; i++){
+            arr[i]=sc.nextInt();
         }
+        
+        System.out.println("Enter the element whose occurance to be find out");
+        int n = sc.nextInt();
+        int count =0;
+        
+        for(int i=0; i<arr.length; i++){
+            if(n==arr[i]){
+                count++;
+            }
+        }
+        System.out.println("The occurance of "+n+" is "+count);
     }
 }
