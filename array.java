f6af2aaa44de6ab9c7b1bb36c8cf622fe9fbc38d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-14 18:40:48 +0530

    Occurance of the element & its count

--- array.java
+++ array.java
@@ -1,30 +1,25 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-// Occurance of the element
+// Occurance of the element & its count
 
 class array {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the array length");
-        int arrlength = sc.nextInt();
-        int [] arr = new int[arrlength];
-
-        System.out.println("Enter the array element form 1-9");
-        for(int i=0; i<arrlength; i++){
-            arr[i]=sc.nextInt();
-        }
-        
-        System.out.println("Enter the element whose occurance to be find out");
-        int n = sc.nextInt();
-        int count =0;
-        
-        for(int i=0; i<arr.length; i++){
-            if(n==arr[i]){
-                count++;
-            }
-        }
-        System.out.println("The occurance of "+n+" is of "+count+" time");
+        int[] arr = { 1, 2, 1, 1, 3, 4, 5, 6, 7, 8, 9 };
+		int maxele = 0;
+		int number = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int count = 0;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+				}
+			}
+			if (count > maxele) {
+				maxele = count;
+				number = arr[i];
+			}
+		}
+		System.out.println("Max occuring number is " + number + " & its count is " + maxele);
     }
 }
