caed27b3d760227c564439313124c7ba1ada67e4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-06 11:44:06 +0530

    Non repeated elements in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
 class ArrayJava{
 	public static void main(String [] args){
-		int [] arr = {1,2,3,4,5,6,5,4,3,2,1};
-
-		int min= arr[0];
-		
+		int [] arr = {1,2,3,4,5,6,7,8,9,10,1,3,5,7,9};
 		for(int i=0; i<arr.length; i++){
-			if(min>arr[i]){
-				min = arr[i];
+			int count = 0;
+			for(int j=0; j<arr.length; j++){
+				if(arr[i]==arr[j]){
+					count++;
+				}
+			}
+			if(count==1){
+				System.out.println(arr[i]);
 			}
 		}
-		System.out.println(min);
 	}
 }
