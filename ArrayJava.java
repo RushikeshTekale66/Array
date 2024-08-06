8b1714522501dd206686469064e71cc285ecc47c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-06 11:42:06 +0530

    Find max element in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,13 +1,14 @@
 class ArrayJava{
 	public static void main(String [] args){
-		int [] arr = {1,2,3,4,5,6,7,8,9,10};
+		int [] arr = {1,2,3,4,5,6,5,4,3,2,1};
+
+		int max = arr[0];
+		
 		for(int i=0; i<arr.length; i++){
-			if(arr[i]%2==0){
-				System.out.println("*");
-			}
-			else{
-				System.out.println("#");
+			if(max<arr[i]){
+				max = arr[i];
 			}
 		}
+		System.out.println(max);
 	}
 }
