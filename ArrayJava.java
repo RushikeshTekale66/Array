f5cfc15379af6e5fb220b1f339d160d05c1c2655
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-06 11:46:10 +0530

    find n th element in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,16 +1,18 @@
+import java.util.Scanner;
 class ArrayJava{
-	public static void main(String [] args){
+	public static void main(String [] args){		
+		Scanner sc = new Scanner(System.in);
 		int [] arr = {1,2,3,4,5,6,7,8,9,10,1,3,5,7,9};
-		for(int i=0; i<arr.length; i++){
-			int count = 0;
-			for(int j=0; j<arr.length; j++){
-				if(arr[i]==arr[j]){
-					count++;
-				}
-			}
-			if(count>1){
-				System.out.println(arr[i]);
-			}
+		
+		System.out.println("Enter the index you want");
+		int index = sc.nextInt();
+		
+		if(index<arr.length){
+			System.out.println(arr[index-1]);
 		}
+		else{
+			System.out.println("Number is greater than array size");
+		}
+		
 	}
 }
