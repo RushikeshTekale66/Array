e98f458e0658a7e8ba502a9867ee9907390b9b84
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-06 11:47:13 +0530

    Add prime number in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,18 +1,28 @@
 import java.util.Scanner;
 class ArrayJava{
-	public static void main(String [] args){		
+	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
-		int [] arr = {1,2,3,4,5,6,7,8,9,10,1,3,5,7,9};
-		
-		System.out.println("Enter the index you want");
-		int index = sc.nextInt();
-		
-		if(index<arr.length){
-			System.out.println(arr[index-1]);
+	
+		int [] arr = new int [5];
+		for(int i=0; i<arr.length; i++){
+			System.out.println("Enter Number for index " + i);
+			int num = sc.nextInt();
+			int count = 0;
+			for(int j=2; j<num; j++){
+				if(num%j==0){
+					count++;
+				}
+			}
+			if(count == 0){
+				arr[i] = num;
+			}
+			else{
+				i--;
+				System.out.println(num + "number is not prime");
+			}
 		}
-		else{
-			System.out.println("Number is greater than array size");
+		for(int i=0; i<arr.length; i++){
+			System.out.println(arr[i]);
 		}
-		
 	}
 }
