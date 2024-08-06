493b425d460294c6351ff712484f16a79a6a79d1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-06 11:39:26 +0530

    Add even number in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
-
 class ArrayJava{
 	public static void main(String [] args){
-		//array Declaration 1
-		int []  arr = {1,2,3,4,5,6,7};
+		Scanner sc = new Scanner(System.in);
+	
+		int [] arr = new int [5];
+		for(int i=0; i<arr.length; i++){
+			System.out.println("Enter Number for index " + i);
+			int num = sc.nextInt();
+			if(num%2==0){
+				arr[i]= num;
+			}
+			else{
+				i--;
+				System.out.println("Odd Number Entered");
+			}
+		}
 		for(int i=0; i<arr.length; i++){
 			System.out.println(arr[i]);
-		}
-
-		//array Declaration 2
-		Scanner sc = new Scanner(System.in);
-		int arr2[] = new int [5];
-		for(int i=0; i<arr2.length; i++){
-			System.out.println("Enter the element at index " + i);
-			arr2[i] = sc.nextInt();
-		}
-		for(int i=0; i<arr2.length; i++){
-			System.out.println(arr2[i]);
 		}
 	}
 }
