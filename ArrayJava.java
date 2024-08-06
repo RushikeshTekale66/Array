f569b08ee2c2d2141409c01e73859c57b1a83c37
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-06 11:43:04 +0530

    Min element in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,14 +1,15 @@
+import java.util.Scanner;
 class ArrayJava{
 	public static void main(String [] args){
 		int [] arr = {1,2,3,4,5,6,5,4,3,2,1};
 
-		int max = arr[0];
+		int min= arr[0];
 		
 		for(int i=0; i<arr.length; i++){
-			if(max<arr[i]){
-				max = arr[i];
+			if(min>arr[i]){
+				min = arr[i];
 			}
 		}
-		System.out.println(max);
+		System.out.println(min);
 	}
 }
