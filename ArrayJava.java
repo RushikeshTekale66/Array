43e992cb846b114339ce6863dd207648cf1a158d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-07 11:38:01 +0530

    Second largest element in array

--- ArrayJava.java
+++ ArrayJava.java
@@ -2,14 +2,14 @@ class ArrayJava{
 	public void sort(int [] arr){
 		for(int i=0; i<arr.length; i++){
 			for(int j=i+1; j<arr.length; j++){
-				if(arr[i]<arr[j]){
+				if(arr[i]>arr[j]){
 					int temp = arr[i];
 					arr[i] = arr[j];
 					arr[j] = temp;
 				}
 			}
-			System.out.print(arr[i] + " ");
 		}
+		System.out.println("Second largest element in array is : " + arr[arr.length-2]);
 		
 	}
 	
@@ -17,7 +17,6 @@ class ArrayJava{
 		int [] arr = {10,30,50,40,60,70,90,80};
 		ArrayJava s = new ArrayJava ();
 		
-		System.out.println("Array in Decending Order");
 		s.sort(arr);
 		
 	}
