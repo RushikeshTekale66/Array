8eb9a2abd8ecdf94917093889d386f26daf679a2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-07 11:40:47 +0530

    Swaping of element without third variable

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,23 +1,19 @@
-class ArrayJava{
-	public void sort(int [] arr){
-		for(int i=0; i<arr.length; i++){
-			for(int j=i+1; j<arr.length; j++){
-				if(arr[i]>arr[j]){
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
-				}
-			}
-		}
-		System.out.println("Second largest element in array is : " + arr[arr.length-2]);
-		
-	}
-	
-	public static void main(String [] args){
-		int [] arr = {10,30,50,40,60,70,90,80};
-		ArrayJava s = new ArrayJava ();
-		
-		s.sort(arr);
-		
-	}
+/**
+ * ArrayJava
+ */
+public class ArrayJava {
+
+	 public static void main(String [] args){
+		int a = 10;
+		int b = 20;
+		System.out.println("Value of a beforeswaping : " + a);
+		System.out.println("Value of b beforeswaping : " + b);
+
+		a = a+b;
+		b = a-b;
+		a = a-b;
+
+		System.out.println("Value of a Afterswaping : " + a);
+		System.out.println("Value of b Afterswaping : " + b);
+	 }
 }
