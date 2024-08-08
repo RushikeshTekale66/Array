9372436c4c4382b5e6b07b343198a48ed6caef23
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-08 12:00:25 +0530

    Largest Odd Element

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,23 +1,22 @@
 class ArrayJava{
-	public void common(int [] arr, int [] arr2){
-		System.out.println("Common element in array is " );
+	public void Odd(int [] arr){
+		System.out.println("Largest Odd element");
+		int max = 0;
+		int element = 0;	
 		for(int i=0; i<arr.length; i++){
-			int count = 0;
-			for(int j=0; j<arr2.length; j++){
-				if(arr[i]==arr2[j]){
-					count ++;
-				}
+			if(arr[i]%2!=0){
+				element = arr[i];
 			}
-			if(count >= 1){
-				System.out.println(arr[i]);
-			}
+			if(max < element){
+			max = element;
+		};
 		}
+		System.out.println(max);
 	}
 	
 	public static void main(String [] args){
-		int [] arr = {1,2,3};
-		int [] arr2 = {1,2, 1, 2,1};
-		ArrayJava m = new ArrayJava();
-		m.common(arr, arr2);
+		int [] arr = {1,2,3,4,5,6,5,4,1,23,8,9,8};
+		ArrayJava n = new ArrayJava();
+		n.Odd(arr);
 	}
 }
