bae600020dddc8040f90ebefe0e76fdc4866f1ec
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-08 12:03:00 +0530

    Max occuring element in java

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,22 +1,21 @@
 class ArrayJava{
-	public void Odd(int [] arr){
-		System.out.println("Largest Odd element");
-		int max = 0;
-		int element = 0;	
-		for(int i=0; i<arr.length; i++){
-			if(arr[i]%2!=0){
-				element = arr[i];
-			}
-			if(max < element){
-			max = element;
-		};
-		}
-		System.out.println(max);
-	}
 	
+	public void max(int arr []){
+		for(int i=0; i<arr.length; i++){
+			int count = 0;
+			for(int j=i+1; j<arr.length; j++){
+				if(arr[i]==arr[j]){
+					count ++;
+				}
+			}
+			if(count==1){
+				System.out.println(arr[i]);
+			}
+		}
+	}
 	public static void main(String [] args){
-		int [] arr = {1,2,3,4,5,6,5,4,1,23,8,9,8};
-		ArrayJava n = new ArrayJava();
-		n.Odd(arr);
+		int [] arr = {1,2,3,1,2,3,1,2,3,4,5,6,7,7,5};
+		ArrayJava m = new ArrayJava();
+		m.max(arr);
 	}
 }
