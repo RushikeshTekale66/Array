73a254b6ca50f1fcd945e75167efc480213b007e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-19 13:02:04 +0530

    Kth element in sorted array

--- ArrayEx.java
+++ ArrayEx.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 class ArrayEx{
@@ -8,7 +7,16 @@ class ArrayEx{
 
 		System.out.println("Enter the index to find");
 		int index = sc.nextInt();
-		Arrays.sort(arr);
+		
+		for(int i=0; i<arr.length; i++){
+			for(int j=i+1; j<arr.length; j++){
+				if(arr[i]>arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
 
 		System.out.println("Element at inde : "+ index + " is " + arr[index-1]);
 	}
